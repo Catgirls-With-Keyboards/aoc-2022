67f5b1bc0ffbd04472689ac2455df50fa490e768
Vi <vi.kitten@proton.me>
2022-12-03 00:21:15 +0000

    vye day 1 [2/2]

---
+++ Day1/vye/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{BufReader,BufRead};
+use std::fs::File;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("../input.txt");
+    let file = BufReader::new(File::open(&path).unwrap());
+    let mut max0: i32 = 0;
+    let mut max1: i32 = 0;
+    let mut max2: i32 = 0;
+    let mut total: i32 = 0;
+    for wrapped_line in file.lines() {
+        let line = wrapped_line.unwrap(); // may panic, should not for valid data
+        if line.is_empty() {
+            if total > max0 { max0 = total; }
+            if total > max1 { max0 = max1; max1 = total; }
+            if total > max2 { max1 = max2; max2 = total; }
+            total = 0;
+        } else {
+            total += line.parse::<i32>().unwrap(); // may panic, should not for valid data
+        }
+    }
+    print!("{}\n", max0 + max1 + max2);
+}
