90a3a85ca71455226da3bd4cd82a39d46f935bc6
Vi <vi.kitten@proton.me>
2022-12-04 02:34:57 +0000

    day 3 vye [1/2]

---
+++ Day3/vye/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("./input.txt");
+    let file = BufReader::new(File::open(&path).unwrap());
+    let mut cost: u32 = 0;
+    for wrapped_line in file.lines() {
+        let line = wrapped_line.unwrap(); // may panic, should not for valid data
+        let segment_len = line.len() / 2;
+        let mut bytes = line.as_str().bytes(); // raw iterator
+        let mut mask: u64 = 0;
+        for _ in 0..segment_len {
+            let byte = bytes.next().unwrap(); // may panic, should not for valid data
+            let score = if byte > 96 {byte - 96} else {byte - 38};
+            mask |= 1 << (score as u64);
+        }
+        'find_overlap: for _ in 0..segment_len {
+            let byte = bytes.next().unwrap(); // may panic, should not for valid data
+            let score = if byte > 96 {byte - 96} else {byte - 38};
+            if mask & (1 << (score as u64)) != 0 {
+                cost += score as u32;
+                break 'find_overlap;
+            }
+        }
+    }
+    print!("{}\n", cost);
+}
